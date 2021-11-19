ea98443a9c2881ac66988bc85ee5f460d00bdb55
Krisch0 <77513447+Krisch0@users.noreply.github.com>
2021-11-19 21:33:55 +0200

    Create database.php

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+$url='localhost';
+$username='naturalm_wp';
+$password='M{_^mSv7gkWQ';
+$conn=mysqli_connect($url,$username,$password,"naturalm_eserbil");
+if(!$conn){
+ die('Could not Connect My Sql:' .mysql_error());
+}
+?>
